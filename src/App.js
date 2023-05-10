b124217f4ef6754a0bf0264cb47ec0c398155bd2
M-Roxana <manolacheroxana96@yahoo.com>
2023-05-10 17:38:18 +0300

    App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Homescreen from "./screens/Homescreen";
+import Bookingscreen from "./screens/Bookingscreen";
+import Registerscreen from "../src/screens/Registerscreen";
+import Loginrscreen from "../src/screens/Loginscreen";
 
 function App() {
   return (
@@ -9,7 +12,14 @@ function App() {
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="home" element={<Homescreen />} />
+          <Route path="home" element={<Homescreen />} exact />
+          <Route
+            path="/book/:roomId/:fromDate/:toDate"
+            element={<Bookingscreen />}
+            exact
+          />
+          <Route path="/register" element={<Registerscreen />} exact />
+          <Route path="/login" element={<Loginrscreen />} exact />
         </Routes>
       </BrowserRouter>
     </div>
