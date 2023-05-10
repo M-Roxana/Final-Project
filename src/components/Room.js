8c25d9a66bef9672957cb8faaed7dc29bb2ff329
M-Roxana <manolacheroxana96@yahoo.com>
2023-05-10 17:36:18 +0300

    Room

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Modal, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Loader from "./Loader";
+import Error from "../components/Error";
 
-function Room({ room }) {
+function Room({ room, fromDate, toDate }) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="row bs">
       <div className="col-md-4">
@@ -15,9 +23,37 @@ function Room({ room }) {
           <p>Type : {room.type}</p>
         </b>
         <div style={{ float: "right" }}>
-          <button className="btn btn-primary">View details</button>
+          <Link to={`/book/${room._id}/${fromDate}/${toDate}`}>
+            <button className="btn btn-primary m-2">Book now</button>
+          </Link>
+          <button className="btn btn-primary" onClick={handleShow}>
+            View details
+          </button>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{room.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel prevLabel="" nextLabel="empty">
+            {room.imageurls.map((url) => {
+              return (
+                <Carousel.Item>
+                  <img className="d-block w-100 bigimg" src={url} />
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+          <p>{room.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
