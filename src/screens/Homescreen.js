a46ef547d899d0646fbb14f51f692c75cade2539
M-Roxana <manolacheroxana96@yahoo.com>
2023-05-10 17:35:29 +0300

    HomeScreen

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,42 +1,77 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Room from "../components/Room";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
+import { DatePicker, Space } from "antd";
+import "antd/dist/reset.css";
+import moment from "moment";
 
 function Homescreen() {
   const [rooms, setrooms] = useState([]);
   const [loading, setloading] = useState();
-  const [error, seterror] = useState();
+  const [error, seterror] = useState(false);
+
+  const { RangePicker } = DatePicker;
+
+  const [fromDate, setFromDate] = useState();
+  const [toDate, setToDate] = useState();
 
   useEffect(() => {
-    async function fetchData() {
+    const fetchData = async () => {
       try {
         setloading(true);
-
         const data = (await axios.get("/api/rooms/getallrooms")).data;
         setrooms(data);
-
         setloading(false);
       } catch (error) {
         seterror(true);
-        console.log(error);
         setloading(false);
+        console.log(rooms);
       }
-    }
+    };
     fetchData();
   }, []);
+
+  function filterByDate(dates) {
+    setFromDate(dates[0].format("DD-MM-YYYY"));
+    setToDate(dates[1].format("DD-MM-YYYY"));
+  }
 
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
-        {loading ? (
-          <h1>Loading...</h1>
+        <div className="col-md-3">
+          <RangePicker format="DD-MM-YYYY" onChange={filterByDate} />
+        </div>
+      </div>
+
+      <div className="row justify-content-center mt-5">
+        {/* {loading ? (
+          <Loader />
+        ) : rooms.length > 1 ? (
+          rooms.map((room, index) => {
+            return (
+              <div className="col-md-9 mt-3" key={index}>
+                <Room room={room} fromDate={fromDate} toDate={toDate} />
+              </div>
+            );
+          })
         ) : error ? (
-          <h1>Error</h1>
+          <Error message="Something went wrong, please try again later" />
+        ) : (
+          <></>
+        )} */}
+
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Error message="Something went wrong, please try again later" />
         ) : (
           rooms.map((room, index) => {
             return (
-              <div className="col-md-9 mt-2" key={index}>
-                <Room room={room} />
+              <div className="col-md-9 mt-3" key={index}>
+                <Room room={room} fromDate={fromDate} toDate={toDate} />
               </div>
             );
           })
