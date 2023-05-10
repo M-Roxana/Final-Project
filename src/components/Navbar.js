313ebc4c36451440ec4bb8a86750f80564a3a14b
M-Roxana <manolacheroxana96@yahoo.com>
2023-05-10 17:36:43 +0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 function Navbar() {
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+
+  function logout() {
+    localStorage.removeItem("currentUser");
+    window.location.href = "/login";
+  }
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Holiday with Views
+          <a className="navbar-brand ml-2" href="/home">
+            <i className="fa-sharp fa-solid fa-mountain-sun "></i> Holiday with
+            Views
           </a>
           <button
             className="navbar-toggler"
@@ -17,20 +25,52 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler">
+              <i className="fa-solid fa-bars" style={{ color: "white" }}></i>
+            </span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/register">
-                  Register
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/login">
-                  Login
-                </a>
-              </li>
+              {user ? (
+                <>
+                  <div className="dropdown mr-5">
+                    <button
+                      className="btn btn-secondary dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <i className="fa-solid fa-user"></i>
+                      {user.name}
+                    </button>
+                    <ul className="dropdown-menu">
+                      <li>
+                        <a className="dropdown-item" href="#">
+                          Bookings
+                        </a>
+                      </li>
+                      <li>
+                        <a className="dropdown-item" href="#" onClick={logout}>
+                          Logout
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/register">
+                      Register
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/login">
+                      Login
+                    </a>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>
