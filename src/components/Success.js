ebe1384035ee5cb3a8b364a8e1aacdfb942097f3
M-Roxana <manolacheroxana96@yahoo.com>
2023-05-10 17:35:59 +0300

    Success

---
+++ src/components/Success.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Success({ message }) {
+  return (
+    <div className="alert alert-success text-center" role="alert">
+      {message}
+    </div>
+  );
+}
+
+export default Success;
